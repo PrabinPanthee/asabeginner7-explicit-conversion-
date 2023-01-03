5ef188403f571fef4f590c4c815820214977f8a2
iampr <iampr@PRABIN>
2023-01-03 22:58:34 +0545

    Add project files.

---
+++ asabeginner7/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+class Program
+{
+    static void Main()
+    {
+        float f = 100000.25F;
+        //cannot implicitly convert float to int
+        //coz fractional part will be lost.Hence loss of data 
+        //as float is bigger data type there is the possibility of
+        //overdflow exception int i = f;
+        //use explicit conversion using cast () operator
+        int i = (int)f;
+        //or use convert class 
+        //int i = convert.Toint32(f);
+        Console.WriteLine(i);   
+    }
+}
